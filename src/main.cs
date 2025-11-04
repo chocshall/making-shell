f5ad114ce8b21f167dab8f911ba6be8f5c3821ad
titanium colaps <titaniumcolaps@gmail.com>
2025-11-04 08:09:15 +0200

    [any message]

--- src/main.cs
+++ src/main.cs
@@ -2,9 +2,16 @@ class Program
 {
     static void Main()
     {
-         //TODO: Uncomment the code below to pass the first stage
-         Console.Write("$ ");
-        if(Console.Write(validCommandsList.Contains(inputCommand));
-        if(Console.Write(validCommandsList.Contains(inputCommand))
+        //TODO: Uncomment the code below to pass the first stage
+        Console.Write("$ ");
+
+        List<string> validCommandsList = new List<string>();
+        validCommandsList.Add("bac");
+        string inputCommand = Console.ReadLine();
+
+        if(!validCommandsList.Contains(inputCommand))
+        {
+            Console.Error.WriteLine(inputCommand + ": command not found");
+        }
     }
 }
