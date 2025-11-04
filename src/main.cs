36475a4683d38a103bc760b4dc2b1a59ef686979
titanium colaps <titaniumcolaps@gmail.com>
2025-11-04 08:04:24 +0200

    changed code to make all commands invalid

--- src/main.cs
+++ src/main.cs
@@ -4,5 +4,7 @@ class Program
     {
          //TODO: Uncomment the code below to pass the first stage
          Console.Write("$ ");
+        if(Console.Write(validCommandsList.Contains(inputCommand));
+        if(Console.Write(validCommandsList.Contains(inputCommand))
     }
 }
