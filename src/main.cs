199fcd919a2846098d08298efc0cd98d902107f5
titanium colaps <titaniumcolaps@gmail.com>
2025-11-04 08:42:00 +0200

    changed code to make run an infinite loop

--- src/main.cs
+++ src/main.cs
@@ -3,15 +3,26 @@ class Program
     static void Main()
     {
         //TODO: Uncomment the code below to pass the first stage
-        Console.Write("$ ");
+        
 
         List<string> validCommandsList = new List<string>();
-        
-        string inputCommand = Console.ReadLine();
 
-        if(!validCommandsList.Contains(inputCommand))
+        while (true)
         {
-            Console.Error.WriteLine(inputCommand + ": command not found");
+            Console.Write("$ ");
+            string inputCommand = Console.ReadLine();
+
+            if (!validCommandsList.Contains(inputCommand))
+            {
+                Console.Error.WriteLine(inputCommand + ": command not found");
+            }
         }
+            
+
+           
+
+           
+
+        
     }
 }
