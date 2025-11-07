e95743f556e34efbfe0c690e3f200b7581b86ad7
titanium colaps <titaniumcolaps@gmail.com>
2025-11-07 11:32:20 +0200

    changed some things want to see how path goes

--- src/main.cs
+++ src/main.cs
@@ -1,3 +1,6 @@
+using System.Diagnostics;
+using System.Formats.Tar;
+using System.IO;
 using System.Net.NetworkInformation;
 
 class Program
@@ -7,101 +10,67 @@ class Program
         //TODO: Uncomment the code below to pass the first stage
 
         bool checker = false;
-        int counter = 0;
+       
         List<string> validCommandsList = new List<string>();
         validCommandsList.Add("exit");
         validCommandsList.Add("echo");
         validCommandsList.Add("type");
 
+        string inputCommand = "";
+        string[] splitInputList =  Array.Empty<string>();
+        //string[] splitInputByCommaList = Array.Empty<string>();
+        
+
+        
+
         while (true)
         {
             Console.Write("$ ");
-            string inputCommand = Console.ReadLine();
-            checker = CheckIfStartsWithCommand(inputCommand, validCommandsList, counter);
 
+            if (!false)
+            {
+
+                inputCommand = Console.ReadLine();
+                splitInputList = inputCommand.Split(' ');
+                checker = CheckIfStartsWithCommand(splitInputList, inputCommand, validCommandsList);
+            }
+
+            else
+            {
+                
+            }
+
+            
+
+            
 
             if (checker)
             {
-                counter++;
                 
-                string[] splitInputList = inputCommand.Split(' ');
+                
+                
 
-                if (splitInputList[0] == "exit")
+                if(splitInputList.Count() > 1)
                 {
-                    foreach (string item in splitInputList)
-                    {
+                    exitCommand(splitInputList, inputCommand);
 
-                        if (splitInputList[0] == "exit" && splitInputList[1] == "0")
-                        {
-                            Environment.Exit(Int32.Parse(splitInputList[1]));
-                        }
-                        // for now probably dont need it
-                        if (splitInputList[0] == "exit" && splitInputList[1] == "1")
-                        {
-                            Environment.Exit(Int32.Parse(splitInputList[1]));
-                        }
-
-                        if (splitInputList[0] == "exit" && Int32.Parse(splitInputList[1]) > 1)
-                        {
-                            Console.Error.WriteLine(inputCommand + ": command not found 1");
-                            break;
-                        }
+                    echoCommand(splitInputList, inputCommand);
 
 
-                    }
+                    typeBuiltCommand(splitInputList, validCommandsList);
                 }
-                    
-                if (splitInputList[0] == "echo")
-                {
-                    if (splitInputList.Count() >= 2)
-                    {
-                        foreach (string item in splitInputList.Skip(1))
-                        {
-                            Console.Write(item + " ");
-                        }
-                        Console.Write("\n");
-                    }
-                    
 
-                     else
-                    {
-                        Console.Error.WriteLine(inputCommand + ": command not found");
-                    }
+                else
+                {
+                    Console.Error.WriteLine(inputCommand + ": command not found");
                 }
 
                 
-
-
-                if (splitInputList[0] == "type")
-                {
-                    bool secondChecker = CheckIfStartsWithCommand(splitInputList[1], validCommandsList, counter);
-
-                    if (secondChecker && splitInputList.Count() == 2)
-                    {
-                        Console.WriteLine(splitInputList[1] + " is a shell builtin");
-                    }
-
-                    if (secondChecker && splitInputList.Count() > 2)
-                    {
-                        Console.Error.WriteLine(splitInputList[1] + ": not found");
-                    }
-
-                    
-                }
-                
-
-                    
-                
-                    
-
-
 
 
             }
 
             
-
-
         }
 
 
@@ -112,7 +81,7 @@ class Program
 
     }
 
-    public static bool CheckIfStartsWithCommand(string inputCommand, List<string> validCommandsList, int counter)
+    public static bool CheckIfStartsWithCommand(string[] splitInputList, string inputCommand, List<string> validCommandsList)
     {
         
         foreach (string item in validCommandsList)
@@ -123,14 +92,95 @@ class Program
             }
 
         }
-        if(counter >= 1)
+        
+        if (splitInputList[0] == "type")
         {
             Console.Error.WriteLine(inputCommand + ": not found");
+           
             return false;
         }
+
         Console.Error.WriteLine(inputCommand + ": command not found");
         
         return false;
+    }
+
+    
+    static void exitCommand(string[] splitInputList, string inputCommand)
+    {
+        if (splitInputList[0] == "exit")
+        {
+            foreach (string item in splitInputList)
+            {
+                if (splitInputList[0] == "exit" && splitInputList[1] == "0")
+                {
+                    Environment.Exit(Int32.Parse(splitInputList[1]));
+                }
+                // for now probably dont need it
+                if (splitInputList[0] == "exit" && splitInputList[1] == "1")
+                {
+                    Environment.Exit(Int32.Parse(splitInputList[1]));
+                }
+
+            }
+
+
+        }
+    }
+
+    static void echoCommand(string[] splitInputList, string inputCommand)
+    {
+        if (splitInputList[0] == "echo")
+        {
+            foreach (string item in splitInputList.Skip(1))
+            {
+                Console.Write(item + " ");
+            }
+            Console.Write("\n");
+
+
+        }
+    }
+
+
+    static void typeBuiltCommand (string[] splitInputList, List<string> validCommandsList)
+    {
+        if (splitInputList[0] == "type")
+        {
+            
+            bool secondChecker = false;
+            
+            secondChecker = CheckIfStartsWithCommand(splitInputList, splitInputList[1], validCommandsList);
+            
+            
+            // probably still need this because (type echo example) can be
+            
+            if (validCommandsList.Contains(splitInputList[1]) && splitInputList.Count() == 2)
+            {
+                Console.WriteLine(splitInputList[1] + " is a shell builtin");
+            }
+
+            string pathListString = Environment.GetEnvironmentVariable("PATH");
+
+            string[] splitPathListString = pathListString.Split(";");
+
+            //if (File.Exists("page.txt"))
+            //{
+            //    Console.WriteLine("iterator exists ");
+            //}
+
+               
+
+
+
+
+            if (splitInputList.Count() > 2)
+            {
+                Console.Error.WriteLine(splitInputList[1] + ": not found");
+            }
+
+
+        }
     }
 
 
