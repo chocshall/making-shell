85a979fa2d35cfee1d8bd731e654fce33f780d6e
titanium colaps <titaniumcolaps@gmail.com>
2025-11-14 11:29:50 +0200

    checking to see what the tester wants

--- src/main.cs
+++ src/main.cs
@@ -38,43 +38,54 @@ class Program
 
     static void userInput(string inputCommand, List<string> validCommandsList, string[] splitInputList)
     {
-        bool checker = false;
+        
         inputCommand = Console.ReadLine();
         splitInputList = inputCommand.Split(' ');
-        checker = CheckIfStartsWithCommand(splitInputList, inputCommand, validCommandsList);
-
-
-
-
-
-        if (checker)
+        
+        if (splitInputList[0].Contains("_exe") && splitInputList.Length > 1)
         {
-
-
-
-
-            if (splitInputList.Count() > 1)
+            splitInputList[0] = splitInputList[0] + ".cs";
+            
+            typeBuiltCommand(splitInputList, validCommandsList, splitInputList[0]);
+        }
+        else
+        {
+            
+            if (splitInputList.Count() > 1 && CheckDoesCommandExist(splitInputList, inputCommand, validCommandsList))
             {
                 exitCommand(splitInputList, inputCommand);
 
                 echoCommand(splitInputList, inputCommand);
 
 
-                typeBuiltCommand(splitInputList, validCommandsList);
+                typeBuiltCommand(splitInputList, validCommandsList, splitInputList[1]);
             }
 
             else
             {
-                Console.Error.WriteLine(inputCommand + ": command not found");
+                Console.Error.WriteLine(inputCommand + ": command not found ");
             }
-
-
-
-
         }
+            
+
+
+
+
+
+        
+
+
+
+
+            
+
+
+
+
+        
     }
 
-    public static bool CheckIfStartsWithCommand(string[] splitInputList, string inputCommand, List<string> validCommandsList)
+    public static bool CheckDoesCommandExist(string[] splitInputList, string inputCommand, List<string> validCommandsList)
     {
         
         foreach (string item in validCommandsList)
@@ -101,7 +112,7 @@ class Program
     
     static void exitCommand(string[] splitInputList, string inputCommand)
     {
-        if (splitInputList[0] == "exit")
+        if (splitInputList[0] == "exit" )
         {
             foreach (string item in splitInputList)
             {
@@ -133,12 +144,13 @@ class Program
 
 
         }
+        
     }
 
-
-    static void typeBuiltCommand (string[] splitInputList, List<string> validCommandsList)
+    // custom_exe arg1 arg2
+    static void typeBuiltCommand (string[] splitInputList, List<string> validCommandsList, string nameOfFile)
     {
-        if (splitInputList[0] == "type")
+        if (splitInputList[0] == "type" || splitInputList[0].Contains("_exe"))
         {
             string[] splitPathList = Array.Empty<string>();
 
@@ -149,7 +161,7 @@ class Program
 
            
 
-            if (!false)
+            if (false)
             {
                 splitPathList = pathListString.Split(Path.PathSeparator,StringSplitOptions.RemoveEmptyEntries);
             }
@@ -157,7 +169,7 @@ class Program
             else
             {
                 //string userInput = $"E:\\Downloads\\testfolder{Path.PathSeparator}E:\\Downloads\\onedollar{Path.PathSeparator}/usr/local/bin{Path.PathSeparator}$PATH";
-                string userInput = $"/usr/bin{Path.PathSeparator}/usr/local/bin{Path.PathSeparator}$PATH";
+                string userInput = $"E:\\Downloads\\Sunshine\\ArgsPrintingFile{Path.PathSeparator}$PATH";
 
 
                 // path variants to check
@@ -166,18 +178,20 @@ class Program
                     .Replace("${PATH}", pathListString)
                     .Replace("%PATH%", pathListString);
                 splitPathList = expandedInput.Split(Path.PathSeparator,StringSplitOptions.RemoveEmptyEntries);
+                
             }
 
             
             
-            string findFileString = splitInputList[1];
+            
             string changedWord = "";
             bool wordCheckerIsPath = false;
 
-            if(!validCommandsList.Contains(splitInputList[1]))
+            if(!validCommandsList.Contains(nameOfFile))
             {
                 foreach(string directoryString in splitPathList)
             {
+                    //Console.WriteLine(directoryString);
                     // skip the not existing directories
                     if (!Directory.Exists(directoryString))
                     {
@@ -185,13 +199,14 @@ class Program
                     }
 
                     // make the full path
-                    changedWord = Path.Join(directoryString, findFileString);
+                    changedWord = Path.Join(directoryString, nameOfFile);
+                    //Console.WriteLine(changedWord);
 
                     //Console.WriteLine(changedWord + "\n");
                     //Console.WriteLine(directoryString);
                     if (File.Exists(changedWord))
                     {
-                        wordCheckerIsPath = true;
+                        
 
                         // checks on linux if the program is executable because file exists is not enoguth to check
                         // thats why there was a problem with finding a file in a folder that you didnt have permis and printed
@@ -205,16 +220,27 @@ class Program
                             {
 
 
-                                Console.WriteLine(findFileString + " is " + changedWord);
-                                wordCheckerIsPath = true;
+                                Console.WriteLine(nameOfFile + " is " + changedWord);
+                                
                                 break;
                             }
                         }
 
+
+
                         if (Path.PathSeparator == ';')
                         {
-                            Console.WriteLine(findFileString + " is " + changedWord);
-                            wordCheckerIsPath = true;
+                            if (splitInputList[0] == "type")
+                            {
+                                Console.WriteLine(nameOfFile + " is " + changedWord);
+                            }
+
+                            else
+                            {
+
+                            }
+                           
+                            
                             break;
                         }
 
@@ -231,11 +257,11 @@ class Program
             }
             
 
-            bool secondChecker = false;
+            
 
             if (!wordCheckerIsPath)
             {
-                secondChecker = CheckIfStartsWithCommand(splitInputList, splitInputList[1], validCommandsList);
+                CheckDoesCommandExist(splitInputList, splitInputList[1], validCommandsList);
             }
                 
             if (validCommandsList.Contains(splitInputList[1]) && splitInputList.Count() == 2)
@@ -256,9 +282,3 @@ class Program
 
 }
 
-
-
-
-// TODO:  make a unit testing for windows and linux?? or does unit testing check both in one heap.
-//  variable  predefined path su unit testing
-// folder where files are made cat or others not using predefined.
