62697c4c1133e2b28f9a8d20831f0f7a8683aa9a
titanium colaps <titaniumcolaps@gmail.com>
2025-12-02 14:38:37 +0200

    now its building but was not specified that the files are in root not in folder

--- .codecrafters/compile.sh
+++ .codecrafters/compile.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 set -e
-cd codecrafters-shell
 dotnet build --configuration Release --output /tmp/codecrafters-build-csharp codecrafters-shell.csproj
--- your_program.sh
+++ your_program.sh
@@ -1,12 +1,3 @@
 #!/bin/sh
-# Build only the main project
-cd codecrafters-shell
-dotnet build codecrafters-shell.csproj --configuration Release --output ./bin
-
-# Run the program
-if [ -f "./bin/codecrafters-shell.dll" ]; then
-    dotnet ./bin/codecrafters-shell.dll "$@"
-else
-    echo "Build failed or output not found in ./bin/"
-    exit 1
-fi  # <-- Missing this!
+dotnet build --configuration Release --output ./bin codecrafters-shell.csproj
+dotnet ./bin/codecrafters-shell.dll "$@"
