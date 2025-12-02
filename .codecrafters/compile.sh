520d6b7197b78e99edf805669c4453c067f3e118
titanium colaps <titaniumcolaps@gmail.com>
2025-12-02 14:06:27 +0200

    again testing why it doesnt build correctly

--- .codecrafters/compile.sh
+++ .codecrafters/compile.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
-#
-# This script is used to compile your program on CodeCrafters
-#
-# This runs before .codecrafters/run.sh
-#
-# Learn more: https://codecrafters.io/program-interface
-
-set -e # Exit on failure
-
+set -e
+cd codecrafters-shell
 dotnet build --configuration Release --output /tmp/codecrafters-build-csharp codecrafters-shell.csproj
